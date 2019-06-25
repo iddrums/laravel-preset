e0df304bc25fa7be1c9165d4c1f50dce1479e5e0
iddrums <=>
2019-06-25 08:57:17 +0100

    Update styles

--- src/Preset.php
+++ src/Preset.php
@@ -8,36 +8,41 @@ use Illuminate\Foundation\Console\Presets\Preset as LaravelPreset;
 
 class Preset extends LaravelPreset
 {
-  public static function install()
- {
-    static::cleanSassDirectory();
-    static::updatePackages();
-    static::updateMix();
-    static::updateScripts();
-  }
-  public static function cleanSassDirectory()
-  {
-       File::cleanDirectory(resource_path('sass'));
-  }
+    public static function install()
+    {
+        static::cleanSassDirectory();
+        static::updatePackages();
+        static::updateMix();
+        static::updateScripts();
+        static::updateStyles();
+        
+    }
 
-  public static function updatePackageArray($packages)
-  {
-    return array_merge(['laravel-mix-tailwind' => '^0.1.0'], Arr::except($packages, [
-        'popper.js',
-        'lodash',
-        'jquery'
-    ]));
-  }
+    public static function updatePackageArray($packages)
+    {
+        return array_merge(['laravel-mix-tailwind' => '^0.1.0'], Arr::except($packages, [
+            'popper.js',
+            'lodash',
+            'jquery',
+            'bootstrap'
+        ]));
+    }
 
-  public static function updateMix()
-  {
-      copy(__DIR__.'/stubs/webpack.mix.js', base_path('webpack.mix.js'));
-  }
+    public static function updateMix()
+    {
+        copy(__DIR__.'/stubs/webpack.mix.js', base_path('webpack.mix.js'));
+    }
 
-  public static function updateScripts()
-  {  
-    copy(__DIR__.'/stubs/app.js', resource_path('js/app.js')); 
-    copy(__DIR__.'/stubs/bootstrap.js', resource_path('js/bootstrap.js')); 
-  }
+    public static function updateScripts()
+    {
+        copy(__DIR__.'/stubs/app.js', resource_path('assets/js/app.js'));
+        copy(__DIR__.'/stubs/bootstrap.js', resource_path('assets/js/bootstrap.js'));
+    }
+
+    public static function updateStyles()
+    {
+        File::cleanDirectory(resource_path('assets/sass'));
+
+        File::put(resource_path('assets/sass/app.sass'), '');
+    }
 }
-
